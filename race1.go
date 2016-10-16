3ac76f2757ab89ed2deff4f358efd05b43ad35a1
Gábor Lipták <gliptak@gmail.com>
2016-10-16 10:35:07 -0400

    Add data race detection example

---
+++ race1.go
@@ -0,0 +1,22 @@
+//
+// From https://golang.org/doc/articles/race_detector.html
+//
+
+package main
+
+import (
+	"sync"
+	"fmt"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i) // Not the 'i' you are looking for.
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
